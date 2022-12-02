b934630d954cad73fb4711a8d60bd303a6fd25b0
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-02 16:53:30 -0600

    Add button component

---
+++ components/FButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-green appearance-none px-5 py-3 rounded-md w-full">
+    <slot />
+  </button>
+
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
