15cf036ad9033326ba5f6cd0ca825a416c0b3da2
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-02 16:54:28 -0600

    Implement basic logic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div class="dark:bg-gray-dark w-full h-full p-4 flex justify-center items-center">
+    <div class="max-w-xs flex flex-col">
+      <div>
+        {{ joke?.setup }}
+      </div>
+      <div v-show="showAnswer">
+        {{ joke?.delivery }}
+      </div>
+      <f-button v-show="!showAnswer" @click="onShowAnswer">Tell me!</f-button>
+      <f-button v-show="showAnswer" @click="onGetJoke">LMAO tell me another one!</f-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import FButton from '../components/FButton.vue';
+import IJoke from '../interfaces/IJoke';
+import getJoke from '../services/getJoke'
+import { useDark, useToggle } from '@vueuse/core'
+
+/* const isDark = useDark()
+useToggle(isDark) */
+const joke = ref<IJoke>()
+const showAnswer = ref<boolean>(false)
 // fetch('https://v2.jokeapi.dev/joke/christmas') ...
+
+onMounted(async () => {
+  joke.value = await getJoke()
+})
+
+const onShowAnswer = () => {
+  showAnswer.value = true
+}
+
+const onGetJoke = async () => {
+  joke.value = await getJoke()
+  showAnswer.value = false
+}
 </script>
