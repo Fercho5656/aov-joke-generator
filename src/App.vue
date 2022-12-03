11727e8fd51671496f249a6365f35b376a311858
Fercho5656 <morenomoranfernando@gmail.com>
2022-12-02 20:02:22 -0600

    Change divs for message component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="dark:bg-gray-dark w-full h-full p-4 flex justify-center items-center">
-    <div class="max-w-xs flex flex-col">
-      <div>
+    <div class="max-w-xs flex flex-col gap-y-3">
+      <message>
         {{ joke?.setup }}
-      </div>
-      <div v-show="showAnswer">
+      </message>
+      <message position="right" v-show="showAnswer">
         {{ joke?.delivery }}
-      </div>
-      <f-button v-show="!showAnswer" @click="onShowAnswer">Tell me!</f-button>
+      </message>
+      <f-button v-show="!showAnswer" @click="onShowAnswer">🤔 Tell me!</f-button>
       <f-button v-show="showAnswer" @click="onGetJoke">LMAO tell me another one!</f-button>
     </div>
   </div>
@@ -15,16 +15,16 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import FButton from '../components/FButton.vue';
+import FButton from './components/FButton.vue';
+import Message from './components/Message.vue';
 import IJoke from '../interfaces/IJoke';
 import getJoke from '../services/getJoke'
 import { useDark, useToggle } from '@vueuse/core'
 
-/* const isDark = useDark()
-useToggle(isDark) */
+const isDark = useDark()
+useToggle(isDark)
 const joke = ref<IJoke>()
 const showAnswer = ref<boolean>(false)
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
 
 onMounted(async () => {
   joke.value = await getJoke()
