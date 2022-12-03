493c5c53e37dedd62d4b925611fabf0110e4d4d7
Fercho5656 <morenomoranfernando@gmail.com>
2022-12-02 20:01:49 -0600

    Add message component

---
+++ src/components/Message.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-green p-5 rounded-lg text-xl w-3/4" :class="position">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">import { computed } from 'vue';
+
+interface Props {
+  position: 'left' | 'right'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  position: 'left'
+})
+
+const position = computed<string>(() => {
+  return props.position === 'left' ? 'self-start' : 'self-end'
+})
+
+</script>
+
+<style scoped>
+
+</style>
